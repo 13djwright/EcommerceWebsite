efefbb4b8207725c90eecc47feebf1540de570ea
Devin Wright <djwr223@cs.uky.edu>
2014-12-01 13:50:09 -0500

    started added templates for future manager and staff functions.

---
+++ statistics.php
@@ -0,0 +1,44 @@
+<html>
+	<head>
+		<title>S&W Home</title>
+		<link rel="stylesheet" type="text/css" href="./stylesheet.css">
+	</head>
+
+	<body>
+		<?php
+			session_start();
+			$liEmail = $liName = $liRole = "";
+			if(!isset($_SESSION['email']) || empty($_SESSION['email'])) {
+				//not logged in
+			}
+			else {
+				$liEmail = $_SESSION['email'];
+				$liName = $_SESSION['firstName'];
+				$liRole = $_SESSION['role'];
+			}
+		?>
+		<h1 id="header">S&W Games and Toys</h1>
+		<div id="main">
+			<div id="links">
+				<?php
+					if($liEmail){
+						echo "<p>Welcome, {$liName}!</p>";
+						echo "<a href='./basket.php'>Basket</a>";
+						echo "<a href='./previous_orders.php'>Orders</a>";
+						if($liRole == "STAFF" || $liRole == "MANAGER") {
+							echo "<a href='./products.php'>Product Details</a>";
+						}
+						if($liRole == "MANAGER") {
+							echo "<a href='./statistics.php'>Statistics</a>";
+						}
+						echo "<a href='./logout.php'>Logout</a>";
+					}
+					else {
+						echo "<a href='./registration.php'>New User Register</a>";
+						echo "<a href='./login.php'>Login</a>";
+					}
+				?>
+			</div>
+		</div>
+	</body>
+</html>
