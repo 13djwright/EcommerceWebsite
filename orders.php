00d99a5032a244a48f4795ba44d3119fc4b3b231
Devin Wright <13djwright@gmail.com>
2014-12-09 00:33:01 -0500

    removed and added to createTables for a better DB. cleaned up some files. added feature to process pending orders as staff/manager.

--- orders.php
+++ orders.php
@@ -49,6 +49,75 @@
 					}
 				?>
 			</div>
+			<h3>Pending Orders</h3>
+			<hr>
+			<div class="pending-orders">
+				<?php
+					//get all the order ids that are pending, and put into an array. loop through those and get all the user information.
+					//after each user info is displayed, use the orderID to display the orderDetails (product name, quantity etc etc)
+					//have a fullfill button on each of these and when pressed, fullfill the order (set date and change quantities ((would require changes to 
+					//$stmt = $conn->prepare("select U.firstName, U.lastName, U.email, U.address, U.zipCode, U.state, O.id from orders O join users U on U.email=O.userEmail where O.dateOrdered is not null and O.dateShipped is null");
+					//$stmt->execute();
+					//$stmt->bind_results($firstName, $lastName, $email, $address, $zipCode, $state, 
+					$orderIDs = array();
+					//order by could go here for oldest/newest
+					$stmt = $conn->prepare("select O.id from orders O where O.dateOrdered is not null and O.dateShipped is null");
+					$stmt->execute();
+					$orderIDtemp = NULL;
+					$stmt->bind_result($orderIDtemp);
+					while($stmt->fetch()) {
+						array_push($orderIDs, $orderIDtemp);
+					}
+					$stmt->close();
+					for($i=0; $i<count($orderIDs); $i++) {
+						echo "<div class='order'>";
+							echo "<form action='process_order.php' method='post'>";
+								//display user info
+								echo "<div class='orderHeader'>";
+									$stmt = $conn->prepare("select U.firstName, U.lastName, U.email, U.address, U.zipCode, U.state, U.city, O.dateOrdered from users U, orders O where U.email=O.userEmail and O.id=?");
+									$stmt->bind_param("i",$orderIDs[$i]);
+									$stmt->execute();
+									$stmt->bind_result($firstName, $lastName, $email, $address, $zipCode, $state, $city, $dateOrdered);
+									$stmt->fetch();
+									$stmt->close();
+									echo "<div class='order-label'>";
+										echo "<span></span>";
+										echo "<span class='to-upper'>{$lastName}, {$firstName}</span>";
+									echo "</div>";
+									echo "<div class='order-label'>";
+										echo "<span class='to-upper'>{$address}</span>";
+										echo "<span class='to-upper'>{$city}, {$state}  {$zipCode}</span>";
+									echo "</div>";
+									echo "<div class='order-label'>";
+										echo "<span>Date Ordered</span>";
+										echo "<span class='to-upper'>{$dateOrdered}</span>";
+									echo "</div>";
+								echo "</div>";
+								//display all products on order
+								echo "<div class='product'>";
+								echo "<span class='product_name'>Name</span>";
+								echo "<span class='product_quantity'>Quantity</span>";
+								echo "<span class='product_price'>Price</span>";
+								echo "</div>";	
+								$stmt = $conn->prepare("select P.name, D.quantity, P.price from products P join orderDetails D on D.productID=P.id where D.orderID=?");
+								$stmt->bind_param("i",$orderIDs[$i]);
+								$stmt->execute();
+								$stmt->bind_result($pName, $pQuantity, $pPrice);
+								while($stmt->fetch()) {
+								echo "<div class='product'>";
+                                echo "<span class='product_name'>{$pName}</span>";
+                                echo "<span class='product_quantity'>{$pQuantity}</span>";
+                                echo "<span class='product_price'>{$pPrice}</span>";
+                                echo "</div>";
+								}
+							echo "<input type='hidden' name='orderID' value='{$orderIDs[$i]}'>";
+							echo "<input type='submit' value='Fullfill Order'>";
+							echo "</form>";
+						echo "</div>";
+					}
+					
+				?>
+			</div>
 		</div>
 	</body>
 </html>
---
+++ process_order.php
@@ -0,0 +1,20 @@
+<?php 
+	session_start();
+	if(!isset($_SESSION['email']) || empty($_SESSION['email'])) {
+		header("Location:./orders.php");
+	}
+	else{
+		include_once("./config.php"); 
+		if(!($_SESSION['role'] == "STAFF" || $_SESSION['role'] == "MANAGER")) {
+			header("Location: ../project/");
+		}
+		$orderID = $_POST['orderID'];
+		$date = date("Y-m-d");
+		$stmt = $conn->prepare("update orders O set O.dateShipped=? where O.id=?");
+		$stmt->bind_param("si", $date, $orderID);
+		$stmt->execute();
+		$stmt->fetch();
+		$stmt->close();
+		header("Location:./orders.php");
+	}
+?>
