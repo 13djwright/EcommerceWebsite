d8696ae121b7535c8b1dc2eaa53e2a3a02fc4ce8
Brandon Stockwell <bgst223@cs.uky.edu>
2014-12-12 02:02:10 -0500

    Finished account settings page. Added password change function and handling. Tested for other bugs on site.

--- account.php
+++ account.php
@@ -65,8 +65,9 @@
 
 					if($_SERVER["REQUEST_METHOD"] == "POST") {
 						if(!empty($_POST["changeAcc"])) {
+							echo "<form method='post' action='./account.php'>";
 							echo "<div id='account_info_title'>Account Type:</div> <div id='account_info_field'>
-								<input type='text' name='account_type' value='{$role}'>
+								$role
 								</div><br class='clear'>";
 							echo "<div id='account_info_title'>First Name:</div> <div id='account_info_field'>
 								<input type='text' name='fname' value='{$fname}'>
@@ -90,12 +91,37 @@
 								<input type='text' name='state' value='{$state}'>
 								</div><br class='clear'>";
 							#Update the DB for the changed fields
-							echo "<form method='post' action='./account.php'>";
+					
 							echo "<input type='submit' value='Confirm Changes' name='confirmAcc' onClick='./account.php'>";
+							echo "</form>";
+
+							echo "<form method='post' action='./account.php'>";
+
+							echo "<div id='account_info_title'>Old Password:</div> <div id='account_info_field'><input type='password' name='oldpassword'></div>
+							<br class='clear'>";
+							echo "<div id='account_info_title'>New Password:</div> <div id='account_info_field'><input type='password' name='newpassword'></div>
+							<br class='clear'>";
+							echo "<div id='account_info_title'>Re-enter:</div> <div id='account_info_field'><input type='password' name='repassword'></div>
+							<br class='clear'>";
+
+							echo "<input type='submit' value='Change Password?' name='PassChange' onClick='./account.php'>";
 							echo "</form>";
 						}
 						else if(!empty($_POST["confirmAcc"])){
-							echo "<span>Changes made to your account information.</span>";
+							echo "<span id='confirm'>Changes made to your account information.</span>";
+							$fname = $_POST['fname'];						
+							$lname = $_POST['lname'];
+							$email = $_POST['email'];
+							$addr = $_POST['address'];
+							$zip = $_POST['zip_code'];
+							$city = $_POST['city'];
+							$state = $_POST['state'];
+
+							$uppstmt = $conn->prepare("UPDATE users405 SET firstName=?, lastName=?, email=?, address=?, city=?, zipCode=?, state=? WHERE email=?");
+							$uppstmt->bind_param("sssssiss", $fname, $lname, $email, $addr, $city, $zip, $state, $liEmail);
+							$uppstmt->execute();
+							$uppstmt->close();
+
 
 							echo "<div id='account_info_title'>Account Type:</div> <div id='account_info_field'>$role</div><br class='clear'>";
 							echo "<div id='account_info_title'>First Name:</div> <div id='account_info_field'>$fname</div><br class='clear'>";
@@ -105,6 +131,70 @@
 							echo "<div id='account_info_title'>Zip Code:</div> <div id='account_info_field'>$zip</div><br class='clear'>";
 							echo "<div id='account_info_title'>City:</div> <div id='account_info_field'>$city</div><br class='clear'>";
 							echo "<div id='account_info_title'>State:</div> <div id='account_info_field'>$state</div><br class='clear'>";
+							
+							echo "<form method='post' action='./account.php'>";
+							echo "<input type='submit' value='Change Account Information' name='changeAcc' onClick='./account.php'>";
+							echo "</form>";
+						}
+						else if(!empty($_POST["PassChange"])){
+							
+
+							$oldPass = $_POST['oldpassword'];
+							$newpass = $_POST['newpassword'];
+							$newpassCheck = $_POST['repassword'];
+
+							if($newpass == "" || $newpass == " "){
+								$newpass = "wrongPass";
+							}
+							if($newpassCheck == "" || $newpassCheck == " "){
+								$newpassCheck = "wrongPass2";
+							}
+							if($oldPass == "" || $oldPass == " "){
+								$oldPass == "wrongPass";
+							}
+
+							$stmt = $conn->prepare("select password from users405 WHERE email=?");
+							$stmt->bind_param("s", $liEmail);
+							$stmt->execute();
+							$stmt->bind_result($currpass);
+							$stmt->fetch();
+							$stmt->close();
+
+							if($newpass != $newpassCheck){
+								echo "<span id='confirm'>New password did not match re-entry of the new password.</span>";
+								echo "<form method='post' action='./account.php'>";
+								echo "<input type='submit' value='Change Account Information' name='changeAcc' onClick='./account.php'>";
+								echo "</form>";
+							}
+							else if($oldPass != $currpass){
+								echo "<span id='confirm'>Incorrect entry for current password.</span>";
+								
+								echo "<form method='post' action='./account.php'>";
+								echo "<input type='submit' value='Change Account Information' name='changeAcc' onClick='./account.php'>";
+								echo "</form>";
+							}
+							else if($newpass == $newpassCheck && $oldPass == $currpass){
+								echo "<span id='confirm'>Password has been changed.</span>";
+								$uppstmt = $conn->prepare("UPDATE users405 SET password=? WHERE email=?");
+								$uppstmt->bind_param("ss", $newpass, $liEmail);
+								$uppstmt->execute();
+								$uppstmt->close();
+
+
+								echo "<div id='account_info_title'>Account Type:</div> <div id='account_info_field'>$role</div><br class='clear'>";
+								echo "<div id='account_info_title'>First Name:</div> <div id='account_info_field'>$fname</div><br class='clear'>";
+								echo "<div id='account_info_title'>Last Name:</div> <div id='account_info_field'>$lname</div><br class='clear'>";
+								echo "<div id='account_info_title'>Email:</div> <div id='account_info_field'>$email</div><br class='clear'>";
+								echo "<div id='account_info_title'>Street Address:</div> <div id='account_info_field'>$addr</div><br class='clear'>";
+								echo "<div id='account_info_title'>Zip Code:</div> <div id='account_info_field'>$zip</div><br class='clear'>";
+								echo "<div id='account_info_title'>City:</div> <div id='account_info_field'>$city</div><br class='clear'>";
+								echo "<div id='account_info_title'>State:</div> <div id='account_info_field'>$state</div><br class='clear'>";
+								
+								echo "<form method='post' action='./account.php'>";
+								echo "<input type='submit' value='Change Account Information' name='changeAcc' onClick='./account.php'>";
+								echo "</form>";
+							}
+
 						}
 					}
 					else{
